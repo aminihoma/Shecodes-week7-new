974cf4229319c671477f6c871ce7e8726995bf66
aminihoma <134835686+aminihoma@users.noreply.github.com>
2023-06-06 18:10:00 +0330

    display forecast data using the API

--- src/index.js
+++ src/index.js
@@ -30,6 +30,14 @@ function formatDate(timestamp) {
 function capitalizeFirstletter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+function getForecast(coordinates) {
+  let apiKey = "502dc8f7ae36e57af1974e18d16a86f8";
+  console.log(coordinates);
+  //https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+  apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 function showTemperature(response) {
   let cityName = response.data.name;
   let cityTemperature = Math.round(response.data.main.temp);
@@ -62,15 +70,13 @@ function showTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   celsiusdegree = cityTemperature;
+  console.log(response.data.coord);
+  getForecast(response.data.coord);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
-  //let units = "metric";
-  //let apiKey = "502dc8f7ae36e57af1974e18d16a86f8";
-  //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=${units}`;
-  //axios.get(apiUrl).then(showTemperature);
   showCity(city);
 }
 
@@ -99,6 +105,7 @@ function showCity(defaultcity) {
   let units = "metric";
   let apiKey = "502dc8f7ae36e57af1974e18d16a86f8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${defaultcity}&appid=${apiKey}&units=${units}`;
+  console.log(apiUrl);
   axios.get(apiUrl).then(showTemperature);
 }
 
@@ -126,4 +133,44 @@ function showCelsius() {
 
   //let convertedDegree = Math.round((constantdegree.innerHTML - 32) / 1.8);
   constantdegree.innerHTML = celsiusdegree;
+}
+function formatDay(timestamp) {
+  date = new Date(timestamp * 1000);
+  console.log(date);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tus", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+                <div class="weather-forecast-date">${formatDay(
+                  forecastDay.dt
+                )}</div>
+                <img
+                  src="https://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
+                  alt=""
+                  width="40"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max">${Math.round(
+                    forecastDay.temp.max
+                  )}°</span>
+                  <span class="weather-forecast-temperature-min">${Math.round(
+                    forecastDay.temp.min
+                  )}°</span>
+                </div>
+              </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + ` </div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
